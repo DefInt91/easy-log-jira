d28d32c844839d3a305493c1f7f99e7b6a34e3e7
Joy <Joy.huang@luanta.com.tw>
2023-11-21 21:25:01 +0800

    version control

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// 讀取在github設定的token
+// 讀取在 github 設定的 token
 // 在 Node.js 中讀取環境變量
 const jiraToken = process.env.JIRA_TOKEN;
 const user = process.env.USER;
@@ -11,61 +11,63 @@ console.log('User:', user);
 console.log('Server:', server);
 console.log('Escalated By:', escalatedBy);
 
-// 以對象形式描述每個選項組
-var selectGroups = [
-    { containerId: 'Summary_container', selectId: 'Summary', dataKey: 'Summary', label: 'Summary' },
-    { containerId: 'comms_manager_container', selectId: 'comms_manager', dataKey: 'comms_manager', label: 'Comms Manager' },
-    { containerId: 'severity_container', selectId: 'severity', dataKey: 'severity', label: 'Severity' },
-    { containerId: 'tier_container', selectId: 'tier', dataKey: 'tier', label: 'Tier' },
-    { containerId: 'RC_container', selectId: 'root_cause', dataKey: 'root_cause', label: 'Root Cause' }
-    // 添加其他選項組的描述...
-];
+if (typeof document !== 'undefined') {
+    // 瀏覽器環境下的 DOM 相關代碼
+    document.addEventListener('DOMContentLoaded', function () {
+        // 以對象形式描述每個選項組
+        var selectGroups = [
+            { containerId: 'Summary_container', selectId: 'Summary', dataKey: 'Summary', label: 'Summary' },
+            { containerId: 'comms_manager_container', selectId: 'comms_manager', dataKey: 'comms_manager', label: 'Comms Manager' },
+            { containerId: 'severity_container', selectId: 'severity', dataKey: 'severity', label: 'Severity' },
+            { containerId: 'tier_container', selectId: 'tier', dataKey: 'tier', label: 'Tier' },
+            { containerId: 'RC_container', selectId: 'root_cause', dataKey: 'root_cause', label: 'Root Cause' }
+            // 添加其他選項組的描述...
+        ];
 
-// 載入 info_config.json
-fetch('info_config.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log('成功載入 JSON 檔案:', data);  // 檢查 data 的實際內容
+        // 載入 info_config.json
+        fetch('info_config.json')
+            .then(response => response.json())
+            .then(data => {
+                console.log('成功載入 JSON 檔案:', data);  // 檢查 data 的實際內容
 
-        // 使用迴圈動態生成選項組
-        selectGroups.forEach(group => {
-            var containerElement = document.getElementById(group.containerId);
-            var selectElement = document.getElementById(group.selectId);
+                // 使用迴圈動態生成選項組
+                selectGroups.forEach(group => {
+                    var containerElement = document.getElementById(group.containerId);
+                    var selectElement = document.getElementById(group.selectId);
 
-            // 檢查是否有指定的屬性
-            if (data && Array.isArray(data[group.dataKey])) {
-                // 添加標籤
-                containerElement.querySelector('label').textContent = group.label;
+                    // 檢查是否有指定的屬性
+                    if (data && Array.isArray(data[group.dataKey])) {
+                        // 添加標籤
+                        containerElement.querySelector('label').textContent = group.label;
 
-                // 生成選項
-                data[group.dataKey].forEach(item => {
-                    var option = document.createElement('option');
-                    option.value = item;
-                    option.text = item;
-                    selectElement.appendChild(option);
+                        // 生成選項
+                        data[group.dataKey].forEach(item => {
+                            var option = document.createElement('option');
+                            option.value = item;
+                            option.text = item;
+                            selectElement.appendChild(option);
+                        });
+                    } else {
+                        console.error(`JSON 檔案格式錯誤：缺少 ${group.dataKey} 屬性或不是陣列`);
+                    }
                 });
-            } else {
-                console.error(`JSON 檔案格式錯誤：缺少 ${group.dataKey} 屬性或不是陣列`);
-            }
-        });
-    })
-    .catch(error => console.error('載入 info_config.json 失敗：', error));
 
-// 在 DOM 加載完成後執行
-document.addEventListener('DOMContentLoaded', function () {
-    // 取得元素
-    var summarySelect = document.getElementById('Summary');
-    var radioSFG = document.getElementById('SFG_Radio');
+                // 取得元素
+                var summarySelect = document.getElementById('Summary');
+                var radioSFG = document.getElementById('SFG_Radio');
 
-    // 監聽事件，當選擇改變時觸發
-    summarySelect.addEventListener('change', function () {
-        // 判斷條件
-        if (summarySelect.value === summarySelect.options[0].value && radioSFG.checked) {
-            // 調用複製到剪貼板的函數
-            copyToClipboard();
-        }
+                // 監聽事件，當選擇改變時觸發
+                summarySelect.addEventListener('change', function () {
+                    // 判斷條件
+                    if (summarySelect.value === summarySelect.options[0].value && radioSFG.checked) {
+                        // 調用複製到剪貼板的函數
+                        copyToClipboard();
+                    }
+                });
+            })
+            .catch(error => console.error('載入 info_config.json 失敗：', error));
     });
-});
+}
 
 function copyToClipboard() {
     var statusElement = document.getElementById("status").value;
@@ -76,21 +78,18 @@ function copyToClipboard() {
     var RootCauseElement = document.getElementById("RootCause").value;
     var SummaryElement = document.getElementById("Summary").value;
 
-
     // 添加條件判斷，如果滿足條件則執行複製到剪貼板的操作
-
     var formattedText = `
-                <b>Status: </b> ${statusElement}<br>
-                <b>Severity: </b> ${severityElement}<br>
-                <b>Name: </b> ${High_Sev_NameElement}<br>
-                <b>Root Cause: </b> ${RootCauseElement}<br>
-                <b>Tier: </b> ${TierElement}<br>
-                <b>Summary: </b> ${SummaryElement}<br>`;
+        <b>Status: </b> ${statusElement}<br>
+        <b>Severity: </b> ${severityElement}<br>
+        <b>Name: </b> ${High_Sev_NameElement}<br>
+        <b>Root Cause: </b> ${RootCauseElement}<br>
+        <b>Tier: </b> ${TierElement}<br>
+        <b>Summary: </b> ${SummaryElement}<br>`;
     var copyTextDiv = document.getElementById("copyText");
     copyTextDiv.innerHTML = formattedText;
     copyTextDiv.focus();
     document.execCommand("selectAll");
     document.execCommand("copy");
     alert("已複製到剪貼板");
-
 }
