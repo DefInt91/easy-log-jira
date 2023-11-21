cbeb98e7b443a7f36c55aaeafa9c23fe5b9ac88e
Joy <Joy.huang@luanta.com.tw>
2023-11-21 21:54:04 +0800

    version control

--- main.js
+++ main.js
@@ -11,63 +11,45 @@ console.log('User:', user);
 console.log('Server:', server);
 console.log('Escalated By:', escalatedBy);
 
+// 載入 info_config.json
+function loadOptions() {
+    fetch('info_config.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log('成功載入 JSON 檔案:', data);  // 檢查 data 的實際內容
+
+            // 使用迴圈動態生成選項組
+            selectGroups.forEach(group => {
+                var containerElement = document.getElementById(group.containerId);
+                var selectElement = document.getElementById(group.selectId);
+
+                // 檢查是否有指定的屬性
+                if (data && Array.isArray(data[group.dataKey])) {
+                    // 添加標籤
+                    containerElement.querySelector('label').textContent = group.label;
+
+                    // 生成選項
+                    data[group.dataKey].forEach(item => {
+                        var option = document.createElement('option');
+                        option.value = item;
+                        option.text = item;
+                        selectElement.appendChild(option);
+                    });
+                } else {
+                    console.error(`JSON 檔案格式錯誤：缺少 ${group.dataKey} 屬性或不是陣列`);
+                }
+            });
+        })
+        .catch(error => console.error('載入 info_config.json 失敗：', error));
+}
+
 if (typeof document !== 'undefined') {
-    // 瀏覽器環境下的 DOM 相關代碼
     document.addEventListener('DOMContentLoaded', function () {
-        // 以對象形式描述每個選項組
-        var selectGroups = [
-            { containerId: 'Summary_container', selectId: 'Summary', dataKey: 'Summary', label: 'Summary' },
-            { containerId: 'comms_manager_container', selectId: 'comms_manager', dataKey: 'comms_manager', label: 'Comms Manager' },
-            { containerId: 'severity_container', selectId: 'severity', dataKey: 'severity', label: 'Severity' },
-            { containerId: 'tier_container', selectId: 'tier', dataKey: 'tier', label: 'Tier' },
-            { containerId: 'RC_container', selectId: 'root_cause', dataKey: 'root_cause', label: 'Root Cause' }
-            // 添加其他選項組的描述...
-        ];
-
-        // 載入 info_config.json
-        fetch('info_config.json')
-            .then(response => response.json())
-            .then(data => {
-                console.log('成功載入 JSON 檔案:', data);  // 檢查 data 的實際內容
-
-                // 使用迴圈動態生成選項組
-                selectGroups.forEach(group => {
-                    var containerElement = document.getElementById(group.containerId);
-                    var selectElement = document.getElementById(group.selectId);
-
-                    // 檢查是否有指定的屬性
-                    if (data && Array.isArray(data[group.dataKey])) {
-                        // 添加標籤
-                        containerElement.querySelector('label').textContent = group.label;
-
-                        // 生成選項
-                        data[group.dataKey].forEach(item => {
-                            var option = document.createElement('option');
-                            option.value = item;
-                            option.text = item;
-                            selectElement.appendChild(option);
-                        });
-                    } else {
-                        console.error(`JSON 檔案格式錯誤：缺少 ${group.dataKey} 屬性或不是陣列`);
-                    }
-                });
-
-                // 取得元素
-                var summarySelect = document.getElementById('Summary');
-                var radioSFG = document.getElementById('SFG_Radio');
-
-                // 監聽事件，當選擇改變時觸發
-                summarySelect.addEventListener('change', function () {
-                    // 判斷條件
-                    if (summarySelect.value === summarySelect.options[0].value && radioSFG.checked) {
-                        // 調用複製到剪貼板的函數
-                        copyToClipboard();
-                    }
-                });
-            })
-            .catch(error => console.error('載入 info_config.json 失敗：', error));
+        // 現在這裡呼叫 loadOptions 函數
+        loadOptions();
     });
 }
+
 
 function copyToClipboard() {
     var statusElement = document.getElementById("status").value;
@@ -85,7 +67,8 @@ function copyToClipboard() {
         <b>Name: </b> ${High_Sev_NameElement}<br>
         <b>Root Cause: </b> ${RootCauseElement}<br>
         <b>Tier: </b> ${TierElement}<br>
-        <b>Summary: </b> ${SummaryElement}<br>`;
+        <b>Summary: </b> ${SummaryElement}<br>
+        <b>Provider: </b> ${radioSFG}<br>`;
     var copyTextDiv = document.getElementById("copyText");
     copyTextDiv.innerHTML = formattedText;
     copyTextDiv.focus();
